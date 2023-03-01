eb88cfac90f4ef25d1b4ae7a289037f51a8f28e9
Joel Hassan <joel.nhn@gmail.com>
2023-03-01 10:15:23 +0200

    use neosolarized theme

--- lua/joel/packer.lua
+++ lua/joel/packer.lua
@@ -13,13 +13,13 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
 
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-	  config = function()
-		  vim.cmd('colorscheme rose-pine')
-	  end
-  })
+  -- use({
+  --     'rose-pine/neovim',
+  --     as = 'rose-pine',
+  --     config = function()
+  --   	  vim.cmd('colorscheme rose-pine')
+  --     end
+  -- })
 
   use({
       "folke/trouble.nvim",
@@ -76,5 +76,9 @@ return require('packer').startup(function(use)
 
   use("tpope/vim-surround")
   use("vim-scripts/ReplaceWithRegister")
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
 end)
 
