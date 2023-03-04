a18226b06970227b642529b95a42037b5e72b5f1
Joel Hassan <joel.nhn@gmail.com>
2023-03-04 16:17:00 +0200

    add remaps for printing

--- after/plugin/refactoring.lua
+++ after/plugin/refactoring.lua
@@ -1,4 +1,12 @@
-require('refactoring').setup({})
+require('refactoring').setup({
+  -- overriding printf statement for cpp
+  print_var_statements = {
+      -- add a custom print var statement for cpp
+      javascript = {
+          'printf("a custom statement %%s %s", %s)'
+      }
+  }
+})
 
 -- Remaps for the refactoring operations currently offered by the plugin
 vim.api.nvim_set_keymap("v", "<leader>re", [[ <Esc><Cmd>lua require('refactoring').refactor('Extract Function')<CR>]],
@@ -23,3 +31,12 @@ vim.api.nvim_set_keymap("n", "<leader>ri", [[ <Cmd>lua require('refactoring').re
 vim.api.nvim_set_keymap("v", "<leader>ri", [[ <Esc><Cmd>lua require('refactoring').refactor('Inline Variable')<CR>]],
     { noremap = true, silent = true, expr = false })
 
+-- Print var
+
+-- Remap in normal mode and passing { normal = true } will automatically find the variable under the cursor and print it
+vim.api.nvim_set_keymap("n", "<leader>clg", ":lua require('refactoring').debug.print_var({ normal = true })<CR>", { noremap = true })
+-- Remap in visual mode will print whatever is in the visual selection
+vim.api.nvim_set_keymap("v", "<leader>clgv", ":lua require('refactoring').debug.print_var({})<CR>", { noremap = true })
+
+-- Cleanup function: this remap should be made in normal mode
+vim.api.nvim_set_keymap("n", "<leader>clgr", ":lua require('refactoring').debug.cleanup({})<CR>", { noremap = true })
