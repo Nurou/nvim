dc527c08ec46038f6643decabdd883db5d50770c
Joel Hassan <joel.nhn@gmail.com>
2023-03-05 19:52:47 +0200

    add telescope buffer settings

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,12 +1,21 @@
 local builtin = require('telescope.builtin')
+local actions = require('telescope.actions')
+
+-- when Telescope is open
+-- <C-x> go to file selection as a split
+-- <C-v> go to file selection as a vsplit
+-- <C-t> go to a file in a new tab
 
 -- extension settings
-vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>o', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ps', function()
+vim.keymap.set('n', '<leader>s', function()
     builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
-vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+vim.keymap.set('n', '<leader>re', builtin.resume, {})
+
 -- recent files remap
 -- Map a shortcut to open the picker.
 vim.api.nvim_set_keymap("n", "<Leader><Leader>",
@@ -19,6 +28,16 @@ require("telescope").setup {
     extensions = {
         recent_files = {
             only_cwd = true
+        },
+    },
+    defaults = {
+        mappings = {
+            i = {
+                ["<C-d>"] = actions.delete_buffer,
+            },
+            n = {
+                ["dd"] = actions.delete_buffer,
+            },
         }
-    }
+    },
 }
