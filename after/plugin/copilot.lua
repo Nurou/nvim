d66b2c3ffe6f15349ce954670e5f5bb790f5a7e4
Joel Hassan <joel.nhn@gmail.com>
2023-04-28 16:24:40 +0300

    change copilot acept cmd

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,2 +1,2 @@
 vim.g.copilot_assume_mapped = true
-vim.api.nvim_set_keymap("i", "/", 'copilot#Accept("<CR>")', {expr=true, silent=true})
+vim.api.nvim_set_keymap("i", "<C-.>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
