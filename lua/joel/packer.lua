1a7df8c7913098ce24313a2c9781843c1e7462af
Joel Hassan <joel.nhn@gmail.com>
2023-04-28 10:58:17 +0300

    set up null-ls and prettier

--- lua/joel/packer.lua
+++ lua/joel/packer.lua
@@ -1,96 +1,97 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
-vim.cmd.packadd('packer.nvim')
+vim.cmd.packadd("packer.nvim")
 
-return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-    use({
-        "folke/trouble.nvim",
-        config = function()
-            require("trouble").setup {
-                icons = false,
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-                -- refer to the configuration section below
-            }
-        end
-    })
-    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-    use("nvim-treesitter/playground")
-    use("theprimeagen/harpoon")
-    use("theprimeagen/refactoring.nvim")
-    use("mbbill/undotree")
-    use("nvim-treesitter/nvim-treesitter-context");
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v1.x',
-        requires = {
-            -- LSP Support
-            { 'neovim/nvim-lspconfig' },
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+	use({
+		"folke/trouble.nvim",
+		config = function()
+			require("trouble").setup({
+				icons = false,
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			})
+		end,
+	})
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use("nvim-treesitter/playground")
+	use("theprimeagen/harpoon")
+	use("theprimeagen/refactoring.nvim")
+	use("mbbill/undotree")
+	use("nvim-treesitter/nvim-treesitter-context")
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v1.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-            -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-buffer' },
-            { 'hrsh7th/cmp-path' },
-            { 'saadparwaiz1/cmp_luasnip' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'hrsh7th/cmp-nvim-lua' },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
 
-            -- Snippets
-            { 'L3MON4D3/LuaSnip' },
-            { 'rafamadriz/friendly-snippets' },
-        }
-    }
-
-    use("folke/zen-mode.nvim")
-    use("github/copilot.vim")
-    use("eandrju/cellular-automaton.nvim")
-    use("laytan/cloak.nvim")
-    use("windwp/nvim-autopairs")
-    use("windwp/nvim-ts-autotag")
-    -- tmux & split window naviator
-    use("christoomey/vim-tmux-navigator")
-    use("vim-scripts/ReplaceWithRegister")
-    use('Tsuzat/NeoSolarized.nvim')
-    use { 'tpope/vim-commentary' } -- Add vim-commentary
-    use 'JoosepAlviste/nvim-ts-context-commentstring'
-    use { 'lewis6991/gitsigns.nvim' }
-    use 'nvim-tree/nvim-web-devicons'
-    -- multi-cursor support :D
-    use 'mg979/vim-visual-multi'
-    use({
-        "kylechui/nvim-surround",
-        tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-        config = function()
-            require("nvim-surround").setup({
-                -- Configuration here, or leave empty to use defaults
-            })
-        end
-    })
-    use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
-    use { 'ibhagwan/fzf-lua', requires = { 'nvim-tree/nvim-web-devicons' } }
-    use { 'junegunn/fzf', run = './install --bin', }
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-    }
-    use {
-        'nvim-tree/nvim-tree.lua',
-        requires = {
-            'nvim-tree/nvim-web-devicons', -- optional
-        },
-    }
-    use "almo7aya/openingh.nvim"
-    use {
-        'ruifm/gitlinker.nvim',
-        requires = 'nvim-lua/plenary.nvim',
-    }
-    use 'kdheepak/lazygit.nvim'
-    use 'PatschD/zippy.nvim'
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("neovim/nvim-lspconfig")
+	use("MunifTanjim/prettier.nvim")
+	use("folke/zen-mode.nvim")
+	use("github/copilot.vim")
+	use("eandrju/cellular-automaton.nvim")
+	use("laytan/cloak.nvim")
+	use("windwp/nvim-autopairs")
+	use("windwp/nvim-ts-autotag")
+	-- tmux & split window naviator
+	use("christoomey/vim-tmux-navigator")
+	use("vim-scripts/ReplaceWithRegister")
+	use("Tsuzat/NeoSolarized.nvim")
+	use({ "tpope/vim-commentary" }) -- Add vim-commentary
+	use("JoosepAlviste/nvim-ts-context-commentstring")
+	use({ "lewis6991/gitsigns.nvim" })
+	use("nvim-tree/nvim-web-devicons")
+	-- multi-cursor support :D
+	use("mg979/vim-visual-multi")
+	use({
+		"kylechui/nvim-surround",
+		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	})
+	use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
+	use({ "ibhagwan/fzf-lua", requires = { "nvim-tree/nvim-web-devicons" } })
+	use({ "junegunn/fzf", run = "./install --bin" })
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "nvim-tree/nvim-web-devicons", opt = true },
+	})
+	use({
+		"nvim-tree/nvim-tree.lua",
+		requires = {
+			"nvim-tree/nvim-web-devicons", -- optional
+		},
+	})
+	use("almo7aya/openingh.nvim")
+	use({
+		"ruifm/gitlinker.nvim",
+		requires = "nvim-lua/plenary.nvim",
+	})
+	use("kdheepak/lazygit.nvim")
+	use("PatschD/zippy.nvim")
 end)
-
