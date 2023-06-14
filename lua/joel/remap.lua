79f07bfd0f389af8520064904084f13ed16ab03e
Joel Hassan <joel.nhn@gmail.com>
2023-06-14 09:38:08 +0300

    disable chmod remap

--- lua/joel/remap.lua
+++ lua/joel/remap.lua
@@ -41,7 +41,8 @@ keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- make current script executable
-keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- TODO: find a better remap for this to avooid accidents
+-- keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
